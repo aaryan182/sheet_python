2bddd8339608d06638351719b88429098e4b18c6
aaryan <aaryanbajaj385@gmail.com>
2025-05-19 16:31:01 +0530

    L287

---
+++ day-2-arrays/4_duplicate_number.py
@@ -0,0 +1,9 @@
+# leetcode 287
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
