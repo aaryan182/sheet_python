be1c8fd160cbbc62f1cc0727bc2a5443fa006c24
aaryan <aaryanbajaj385@gmail.com>
2025-05-19 09:14:31 +0530

    L53

---
+++ day-1-arrays/4_MaxSubArr.py
@@ -0,0 +1,11 @@
+# Leetcode 53
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            current_sum = max(nums[i], current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum
