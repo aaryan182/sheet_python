f4826b9f80c105ba63f29c3aaa2204abe5824a64
aaryan <aaryanbajaj385@gmail.com>
2025-05-19 16:32:20 +0530

    Optimal Approach added L287

--- day-2-arrays/4_duplicate_number.py
+++ day-2-arrays/4_duplicate_number.py
@@ -7,3 +7,25 @@ class Solution:
             if num in seen:
                 return num
             seen.add(num)
+            
+            
+# Optimal Approach
+
+def findDuplicate(nums):
+    # Step 1: Detect cycle using slow and fast pointers
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+
+    # Step 2: Find the entrance to the cycle
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+
+    return slow
