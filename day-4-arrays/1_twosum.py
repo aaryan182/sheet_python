984e97e673fc6e75286a520167a51926896866c4
aaryan <aaryanbajaj385@gmail.com>
2025-05-20 14:48:53 +0530

    L1

---
+++ day-4-arrays/1_twosum.py
@@ -0,0 +1,10 @@
+# leetcode 1
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
