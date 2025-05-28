66a2f86cb60936572b7c54993de7b9d108eb9eaa
aaryan <aaryanbajaj385@gmail.com>
2025-05-28 14:21:37 +0530

    TUF sheet question

---
+++ day-23-Graph/2-dfs.py
@@ -0,0 +1,30 @@
+# Traversal Techniques
+
+# Given an undirected connected graph with V vertices numbered from 0 to V-1, the task is to implement both Depth First Search (DFS) and Breadth First Search (BFS) traversals starting from the 0th vertex. The graph is represented using an adjacency list where adj[i] contains a list of vertices connected to vertex i. Visit nodes in the order they appear in the adjacency list.
+
+# Examples:
+
+# Input: V = 5, adj = [[2, 3, 1], [0], [0, 4], [0], [2]]
+
+# Output:[0, 2, 4, 3, 1], [0, 2, 3, 1, 4]
+
+# Explanation:
+
+# DFS: Start from vertex 0. Visit vertex 2, then vertex 4, backtrack to vertex 0, then visit vertex 3, and finally vertex 1. The traversal is 0 → 2 → 4 → 3 → 1.
+
+# BFS: Start from vertex 0. Visit vertices 2, 3, and 1 (in the order they appear in the adjacency list). Then, visit vertex 4 from vertex 2. The traversal is 0 → 2 → 3 → 1 → 4.
+
+
+def dfs_traversal(V, adj):
+    visited = [False] * V
+    result = []
+    
+    def dfs(node):
+        visited[node] = True
+        result.append(node)
+        for neighbor in adj[node]:
+            if not visited[neighbor]:
+                dfs(neighbor)
+    
+    dfs(0)
+    return result
