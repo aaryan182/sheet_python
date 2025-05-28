ba221297b39d1d0db0341292934a6772efaaec34
aaryan <aaryanbajaj385@gmail.com>
2025-05-28 14:22:04 +0530

    L207

---
+++ day-23-Graph/4-cycleUndirectedGraphBFS.py
@@ -0,0 +1,23 @@
+# leetcode 207
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+            in_degree[a] += 1
+
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        count = 0
+
+        while queue:
+            course = queue.popleft()
+            count += 1
+            for neighbor in graph[course]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        return count == numCourses
